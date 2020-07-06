85f63f8fd09b7718f50dfebca8a8083f7a60a560
pmlh121 <pmlh121@gmail.com>
2020-07-06 16:14:41 +0800

    初始化

---
+++ VolvoS90PlayListMaker/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace VolvoS90PlayListMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TextBox_HintMessage = new System.Windows.Forms.RichTextBox();
+            this.TextBox_Result = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Button_Run = new System.Windows.Forms.Button();
+            this.Button_Rescan = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "选择U盘盘符：";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(207, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "生成播放列表：";
+            // 
+            // TextBox_HintMessage
+            // 
+            this.TextBox_HintMessage.Location = new System.Drawing.Point(33, 347);
+            this.TextBox_HintMessage.Name = "TextBox_HintMessage";
+            this.TextBox_HintMessage.Size = new System.Drawing.Size(436, 160);
+            this.TextBox_HintMessage.TabIndex = 3;
+            this.TextBox_HintMessage.Text = "";
+            // 
+            // TextBox_Result
+            // 
+            this.TextBox_Result.Location = new System.Drawing.Point(33, 107);
+            this.TextBox_Result.Name = "TextBox_Result";
+            this.TextBox_Result.Size = new System.Drawing.Size(436, 195);
+            this.TextBox_Result.TabIndex = 3;
+            this.TextBox_Result.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 318);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "使用帮助：";
+            // 
+            // Button_Run
+            // 
+            this.Button_Run.Location = new System.Drawing.Point(207, 61);
+            this.Button_Run.Name = "Button_Run";
+            this.Button_Run.Size = new System.Drawing.Size(75, 23);
+            this.Button_Run.TabIndex = 4;
+            this.Button_Run.Text = "生成";
+            this.Button_Run.UseVisualStyleBackColor = true;
+            this.Button_Run.Click += new System.EventHandler(this.Button_Run_Click);
+            // 
+            // Button_Rescan
+            // 
+            this.Button_Rescan.Location = new System.Drawing.Point(335, 21);
+            this.Button_Rescan.Name = "Button_Rescan";
+            this.Button_Rescan.Size = new System.Drawing.Size(75, 23);
+            this.Button_Rescan.TabIndex = 5;
+            this.Button_Rescan.Text = "重新扫描";
+            this.Button_Rescan.UseVisualStyleBackColor = true;
+            this.Button_Rescan.Click += new System.EventHandler(this.Button_Rescan_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(288, 66);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 12);
+            this.label6.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 538);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Button_Rescan);
+            this.Controls.Add(this.Button_Run);
+            this.Controls.Add(this.TextBox_Result);
+            this.Controls.Add(this.TextBox_HintMessage);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VolvoS90车机播放列表生成器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox TextBox_HintMessage;
+        private System.Windows.Forms.RichTextBox TextBox_Result;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button Button_Run;
+        private System.Windows.Forms.Button Button_Rescan;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ VolvoS90PlayListMaker/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/* Author:liheng四十斤莴笋
+ * Date:2020-07-04
+ * 用于给存放音乐文件的U盘生成适用于Volvo S90 车机支持的播放列表
+*/
+
+namespace VolvoS90PlayListMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeHintMessage();
+            InitializeDiskList();
+
+        }
+
+        /// <summary>
+        /// 初始化提示信息
+        /// </summary>
+        private void InitializeHintMessage()
+        {
+            this.TextBox_HintMessage.Text = "1，在U盘根目录按文件夹分类，歌曲存放在不同的文件夹内（可以有子文件夹）。";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "2，将U盘插到电脑上。";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "3，在电脑上运行此软件。（若无法识别U盘，请点击重新识别）";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "4，点击“生成”，并确认U盘根目录已根据文件夹生成相应的播放列表文件（m3u8）";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "5，关闭软件，拔出U盘。";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "6，U盘插到车载播放器上。";
+            this.TextBox_HintMessage.Text += System.Environment.NewLine + "7，在车机上选择U盘，并点击“库”，选择“播放列表即可”。";
+        }
+
+        /// <summary>
+        /// 初始化盘符下拉列表
+        /// </summary>
+        private int InitializeDiskList()
+        {
+            DriveInfo[] allDrivers = DriveInfo.GetDrives();
+            List<DriveInfo> removableDrivers = new List<DriveInfo>();
+            //检索计算机上的所有逻辑驱动器名称
+            foreach (DriveInfo item in allDrivers)
+            {
+                if (item.DriveType == DriveType.Removable)
+                {
+                    removableDrivers.Add(item);
+                }
+            }
+
+            this.comboBox1.Items.AddRange(removableDrivers.ToArray());
+            if (removableDrivers.Count > 0)
+            {
+                this.comboBox1.SelectedIndex = 0;
+            }
+            return removableDrivers.Count;
+        }
+
+
+
+        /// <summary>
+        /// 启动
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Run_Click(object sender, EventArgs e)
+        {
+            //button1_Click(sender, e);
+            //return;
+
+
+            //清空提示消息与结果列表
+            this.TextBox_Result.Text = this.label6.Text = "";
+            if (this.comboBox1.SelectedItem == null)
+            {
+                this.label6.Text = "请选择盘符。";
+                return;
+            }
+            DriveInfo disk = (DriveInfo)this.comboBox1.SelectedItem;
+            DirectoryInfo TheFolder = new DirectoryInfo(disk.Name);
+            if (!TheFolder.Exists)
+            {
+                this.label6.Text = "请选择盘符。";
+                return;
+            }
+            //清除已有的播放列表（m3u8文件）
+            FileInfo[] playList = TheFolder.GetFiles();
+            foreach (FileInfo fi in playList)
+            {
+                if (fi.Extension.ToLower().Equals(".m3u"))
+                {
+                    File.Delete(fi.FullName);
+                }
+            }
+            //获取文件夹列表
+            DirectoryInfo[] directortyList = TheFolder.GetDirectories();
+            //遍历每个文件夹
+            foreach (DirectoryInfo dr in directortyList)
+            {
+                if (dr.Name == "System Volume Information")
+                    continue;
+                List<string> playListData = new List<string>();
+                //创建播放列表文件
+                new FileInfo(dr.FullName + ".m3u").Create().Close();
+                //遍历文件夹（递归查找）
+                GetFilesRecursion(dr.FullName, dr.Name, ref playListData);
+
+                foreach (string musicName in playListData)
+                {
+                    this.TextBox_Result.AppendText(musicName + System.Environment.NewLine);
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(dr.FullName + ".m3u", true, Encoding.GetEncoding("GB2312")))
+                    //using (System.IO.StreamWriter file = new System.IO.StreamWriter(dr.FullName + ".m3u8", true))
+                    {
+                        file.WriteLine(musicName);// 直接追加文件末尾，换行
+                    }
+                }
+            }
+            this.label6.Text = "处理完成！";
+        }
+
+        /// <summary>
+        /// 重新扫描
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Rescan_Click(object sender, EventArgs e)
+        {
+            this.comboBox1.Items.Clear();
+            int DiskCount = InitializeDiskList();
+            //MessageBox.Show("找到" + DiskCount.ToString() + "个可移动磁盘");
+            if (DiskCount > 0)
+            {
+                this.comboBox1.SelectedIndex = 0;
+            }
+        }
+
+        private void GetFilesRecursion(string fullpathname, string pathname, ref List<string> list)
+        {
+            DirectoryInfo TheFolder = new DirectoryInfo(fullpathname);
+            FileInfo[] fileList = TheFolder.GetFiles();
+            foreach (FileInfo filename in fileList)
+            {
+                list.Add(pathname + "\\" + filename.Name);
+            }
+
+            DirectoryInfo[] folderList = TheFolder.GetDirectories();
+            foreach (DirectoryInfo folder in folderList)
+            {
+                GetFilesRecursion(fullpathname + "\\" + folder.Name, pathname + "\\" + folder.Name, ref list);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// 新建线程并执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart thStart = new ThreadStart(Pro);//threadStart委托 
+            Thread thread = new Thread(thStart);
+            thread.Priority = ThreadPriority.Highest;
+            thread.IsBackground = true; //关闭窗体继续执行
+            thread.Start();
+        }
+
+        public void Pro()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                for (int j = 0; j < 100000; j++)
+                {
+                    if (TextBox_Result.InvokeRequired)//不同线程访问了
+                        TextBox_Result.Invoke(new Action<RichTextBox, string>(SetTxtValue), TextBox_Result, j.ToString());//跨线程了
+                    else//同线程直接赋值
+                        TextBox_Result.Text = j.ToString();
+                }
+            }
+        }
+
+        private void SetTxtValue(RichTextBox txt, string value)
+        {
+            txt.Text = value;
+        }
+    }
+}
